2bb2135c2e8c2ee011874c1710fc7e8abc02c658
刘阳 <464585657@qq.com>
2018-09-26 17:23:18 +0800

    工程

--- Db.cs
+++ Db.cs
@@ -5,31 +5,76 @@ using System.Text;
 
 using MySql.Data.MySqlClient;
 
-namespace XglDaService
+namespace Tz
 {
-    class Db
+    public class Db
     {
-        public static string connection_string { get; set; }
-        public static string table_refix { get; set; }
-        public const string TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
-        private string __table_name;
-        private List<Dictionary<string, string>> __join;
-        private List<string> __where;
-        private string __order_field_name;
-        private string __order_sort_method;
-        private string __fields;
-
-        protected string _last_sql = "";
-
-        public Db(string conn_str)
+        /** 连接字符串
+         */ 
+        public static string connection_string
         {
-            if (string.IsNullOrEmpty(conn_str) || string.IsNullOrEmpty(conn_str.Trim()))
+            get { return _connection_string; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()))
+                    throw new Exception("空字符串！");
+                _connection_string = value.Trim();
+            }
+        }
+        protected static string _connection_string = "Server=127.0.0.1;Database=test; User ID=root;Password=123456;port=3306;CharSet=utf8;pooling=true;";
+
+        /** 表前缀
+         */ 
+        public static string table_prefix
+        {
+            get { return _table_prefix; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()))
+                    _table_prefix = "";
+                else
+                    _table_prefix = value.Trim();
+            }
+        }
+        protected static string _table_prefix = "";
+
+        /** DATETIME字段格式化字符串
+         */
+        public static string format_datetime
+        {
+            get { return _format_datetime; }
+            set
+            {
+                if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()))
+                    throw new Exception("空字符串！");
+                _format_datetime = value.Trim();
+            }
+        }
+        protected static string _format_datetime = "yyyy-MM-dd HH:mm:ss";
+
+        /** 上次执行的SQL语句
+         */ 
+        public static string last_sql
+        {
+            get { return _last_sql; }
+        }
+        protected static string _last_sql = "";
+
+        /** 查询使用的临时变量
+         */
+        private string __table_name = ""; //FROM表名
+        private List<Dictionary<string, string>> __join; //JOIN条件
+        private List<string> __where; //WHERE条件
+        private string __order_field_name; //ORDER字段
+        private string __order_sort_method; //ORDER方法
+        private string __fields = "*"; //SELECT字段
+
+        /** 构造函数
+         */ 
+        private Db()
+        {
+            if (string.IsNullOrEmpty(_connection_string))
                 throw new Exception("空连接字符串！");
-            conn_str = conn_str.Trim();
-            connection_string = conn_str;
-            if (string.IsNullOrEmpty(table_refix))
-                table_refix = "";
             __table_name = "";
             __join = new List<Dictionary<string, string>>();
             __where = new List<string>();
@@ -38,32 +83,45 @@ namespace XglDaService
             __fields = "*";
         }
 
+        #region 参数
+
+        /** FROM 表名（不带前缀）
+         */
         public static Db Name(string name)
         {
-            if (string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(name))
                 throw new Exception("空表名！");
+            name = name.Trim();
             Db ms = new Db();
-            ms.__table_name = table_refix + name;
+            ms.__table_name = _table_prefix + name;
             return ms;
         }
 
+        /** FROM 完整表名（带前缀）
+         */
         public static Db Table(string table_name)
         {
-            if (string.IsNullOrEmpty(table_name))
+            if (string.IsNullOrEmpty(table_name) || string.IsNullOrEmpty(table_name))
                 throw new Exception("空表名！");
+            table_name = table_name.Trim();
             Db ms = new Db();
             ms.__table_name = table_name;
             return ms;
         }
 
+        /** JOIN 完整表名
+         */ 
         public Db Join(string table_name, string join_condition, string join_method)
         {
-            if (string.IsNullOrEmpty(table_name))
+            if (string.IsNullOrEmpty(table_name) || string.IsNullOrEmpty(table_name))
                 throw new Exception("空表名！");
-            if (string.IsNullOrEmpty(join_condition))
+            table_name = table_name.Trim();
+            if (string.IsNullOrEmpty(join_condition) || string.IsNullOrEmpty(join_condition))
                 throw new Exception("空条件！");
-            if (string.IsNullOrEmpty(join_method))
+            join_condition = join_condition.Trim();
+            if (string.IsNullOrEmpty(join_method) || string.IsNullOrEmpty(join_method))
                 throw new Exception("空方式！");
+            join_method = join_method.Trim();
             Dictionary<string, string> dic = new Dictionary<string, string>();
             dic.Add("table_name", table_name);
             dic.Add("join_condition", join_condition);
@@ -72,37 +130,58 @@ namespace XglDaService
             return this;
         }
 
+        /** WHERE 条件
+         */ 
         public Db Where(string where_condition)
         {
-            if (string.IsNullOrEmpty(where_condition))
+            if (string.IsNullOrEmpty(where_condition) || string.IsNullOrEmpty(where_condition.Trim()))
                 throw new Exception("空条件！");
+            where_condition = where_condition.Trim();
             __where.Add(where_condition);
             return this;
         }
 
-        public Db Order(string field_name, string sort_method)
+        /** ORDER 子句
+         */ 
+        public Db Order(string field_name, string order_method)
         {
-            if (string.IsNullOrEmpty(field_name))
+            if (string.IsNullOrEmpty(field_name) || string.IsNullOrEmpty(field_name.Trim()))
                 throw new Exception("空字段！");
-            if (string.IsNullOrEmpty(sort_method))
+            field_name = field_name.Trim();
+            if (string.IsNullOrEmpty(order_method) || string.IsNullOrEmpty(order_method.Trim()))
                 throw new Exception("空排序方式！");
+            order_method = order_method.Trim();
+            string em = order_method.ToUpper();
+            if(em != "ASC" && em != "DESC")
+                throw new Exception("排序方式有误！");
             __order_field_name = field_name;
-            __order_sort_method = sort_method;
+            __order_sort_method = order_method;
             return this;
         }
 
+        /** SELECT 哪些字段
+         */ 
         public Db Field(string fields)
         {
-            if (string.IsNullOrEmpty(fields))
+            if (string.IsNullOrEmpty(fields) || string.IsNullOrEmpty(fields.Trim()))
                 throw new Exception("空字段！");
+            fields = fields.Trim();
             __fields = fields;
             return this;
         }
 
+        #endregion
+
+        #region 动作
+
+        /** 查询第一条记录
+         */ 
         public Dictionary<string, object> Find()
         {
             if (string.IsNullOrEmpty(__table_name))
                 throw new Exception("空表名！");
+            if (string.IsNullOrEmpty(__fields))
+                __fields = "*";
             StringBuilder sb = new StringBuilder();
             sb.Append("SELECT ").Append(__fields).Append(" FROM `").Append(__table_name).Append("`");
             if (__join.Count > 0)
@@ -159,10 +238,14 @@ namespace XglDaService
             return (ret.Count > 0 ? ret : null);
         }
 
+        /** 查询
+         */ 
         public List<Dictionary<string, object>> Select()
         {
             if (string.IsNullOrEmpty(__table_name))
                 throw new Exception("空表名！");
+            if (string.IsNullOrEmpty(__fields))
+                __fields = "*";
             StringBuilder sb = new StringBuilder();
             sb.Append("SELECT ").Append(__fields).Append(" FROM `").Append(__table_name).Append("`");
             if (__join.Count > 0)
@@ -221,12 +304,15 @@ namespace XglDaService
             return (ret.Count > 0 ? ret : null);
         }
 
+        /** 删除记录（必须设置WHERE条件才允许删除）
+         *  返回：受影响的行数
+         */ 
         public int Delete()
         {
             if (string.IsNullOrEmpty(__table_name))
                 throw new Exception("空表名！");
             if (__where.Count == 0)
-                throw new Exception("没有设置删除where条件！");
+                throw new Exception("没有设置WHERE条件！");
             StringBuilder sb = new StringBuilder();
             sb.Append("DELETE FROM `").Append(__table_name).Append("`");
             if (__where.Count > 0)
@@ -267,6 +353,9 @@ namespace XglDaService
             return ret;
         }
 
+        /** 添加记录
+         *  返回：受影响的行数
+         */ 
         public int Add(Dictionary<string, string> data)
         {
             if (string.IsNullOrEmpty(__table_name))
@@ -318,12 +407,14 @@ namespace XglDaService
             return ret;
         }
 
+        /** 更改记录（必须设置WHERE条件）
+         */ 
         public int Update(Dictionary<string, string> data)
         {
             if (string.IsNullOrEmpty(__table_name))
                 throw new Exception("空表名！");
             if (__where.Count == 0)
-                throw new Exception("没有设置更新的where条件！");
+                throw new Exception("没有设置WHERE条件！");
             if (data.Count == 0)
                 throw new Exception("无字段！");
             StringBuilder sb = new StringBuilder();
@@ -373,5 +464,7 @@ namespace XglDaService
             }
             return ret;
         }
+
+        #endregion
     }
 }
