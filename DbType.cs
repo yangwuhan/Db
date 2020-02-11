af033d08b86e0f322429883808c87283ec7b9b5d
刘阳 <464585657@qq.com>
2020-02-11 09:45:52 +0800

    sqlite

---
+++ DbType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tz
+{
+    public enum EDbType
+    {
+        MYSQL = 1,
+        SQLITE = 2
+    }
+}
